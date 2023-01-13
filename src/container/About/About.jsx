f23488b5dca7aa28f023c7fc09bf755a2e0559e6
Luke Li <lukeli@Lukes-MacBook-Pro.local>
2023-01-12 16:42:37 -0800

    complete work section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -17,6 +17,7 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">I Know That <span>Good Development</span> <br /> means <span>Good Business</span></h2>
+      
       <div className="app__profiles">
         { abouts.map((about, index) => (
           <motion.div
