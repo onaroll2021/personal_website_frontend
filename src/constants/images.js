9c382ef371d937e632102076e5003861cfcb97cd
Luke Li <lukeli@Lukes-MacBook-Pro.local>
2023-01-15 19:52:40 -0800

    modify content

--- src/constants/images.js
+++ src/constants/images.js
@@ -17,6 +17,8 @@ import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
+import php from '../assets/php.png';
+import ruby from '../assets/ruby.png';
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -36,6 +38,8 @@ import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 
 export default {
+  ruby,
+  php,
   email,
   mobile,
   api,
